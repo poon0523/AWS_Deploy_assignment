b16cd2e0486485ebeb5c17171212f3247195d625
maiko <urbanrepablic@gmail.com>
2023-12-20 08:48:40 +0900

    add s3

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'unicorn'
 
+gem 'aws-sdk-s3', require: false
+gem 'dotenv-rails'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,11 +2,12 @@ lock "~> 3.10"    # 1
 
 set :application, "blog_app"    # 2
 set :repo_url, "https://github.com/poon0523/AWS_Deploy_assignment.git"    # 3
-set :linked_files, %w{config/secrets.yml}   # 4
+set :linked_files, %w{config/secrets.yml .env}   # 4
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}   # 5
 set :keep_releases, 5   # 6
 set :rbenv_ruby, '3.0.1'    # 7
 set :log_level, :info   # 8
+
 
 after 'deploy:published', 'deploy:seed'   # 9
 after 'deploy:finished', 'deploy:restart'   # 10
