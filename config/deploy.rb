1ed5a46aadaa373921974de485a8446076f31bd8
maiko <urbanrepablic@gmail.com>
2023-12-20 08:29:21 +0900

    add unicorn and nginx

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 lock "~> 3.10"    # 1
 
-set :application, "task_app"    # 2
+set :application, "blog_app"    # 2
 set :repo_url, "https://github.com/poon0523/AWS_Deploy_assignment.git"    # 3
 set :linked_files, %w{config/secrets.yml}   # 4
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}   # 5
